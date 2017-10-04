984f684dda6eb8991de9ee7906c02b7fffe91df1
Jason Gardner <im@jasongardner.co>
2017-10-04 18:20:36 -0500

    Added bit masks for excluding start/end dates
    Replaced the `$excludeStartDate` and `$excludeEndDate` properties and methods with bit mask flags in parameters.

--- src/DateRange.php
+++ src/DateRange.php
@@ -12,8 +12,7 @@ use DateTime,
 	DatePeriod,
 	IteratorAggregate,
 	Exception,
-	InvalidArgumentException,
-	LogicException;
+	InvalidArgumentException;
 
 /**
  * Date Range object
@@ -22,55 +21,51 @@ use DateTime,
 class DateRange implements IteratorAggregate
 {
 	/**
+	 * Flag to make date range exclude the start date
+	 */
+	const EXCLUDE_START_DATE = 0b0001;
+
+	/**
+	 * Flag to make the date range exclude the end date
+	 */
+	const EXCLUDE_END_DATE = 0b0010;
+
+	/**
 	 * Earliest date in date range
 	 * @var \DateTime
 	 */
-    private $startDate;
+	private $startDate;
 
 	/**
 	 * Latest date in date range
 	 * @var \DateTime
 	 */
-    private $endDate;
+	private $endDate;
 
 	/**
-	 * Time period between start and end dates
+	 * Interval used when iterating over date range or getting date period
 	 * @var \DateInterval
 	 */
-    private $interval;
-
-	/**
-	 * Flag to exclude start date in date period
-	 * @var bool
-	 */
-    private $excludeStartDate = false;
-
-	/**
-	 * Flag to exclude end date in date period
-	 * @var bool
-	 */
-    private $excludeEndDate = false;
-
-	/**
-	 * Default interval to use when determining date range period
-	 */
-    private const INTERVAL = 'P1D';
+	private $interval;
 
 	/**
 	 * DateRange constructor.
 	 * Pairs two `\DateTime` objects as a date range
 	 *
 	 * @param array|string|int|\DateTime $startDate Earliest date to include in range.
-	 * @param string|int|null|\DateTime $endDate Latest date to include in range. May be omitted if `$startDate` is
-	 *                                           an array with at least two elements.
-	 * @throws \Exception An `\Exception` is thrown if `\DateInterval` cannot parse `\jasonjgardner\DateRange::INTERVAL`
+	 * @param string|int|null|\DateTime  $endDate   Latest date to include in range. May be omitted if `$startDate` is
+	 *                                              an array with at least two elements.
+	 * @param string                     $interval  Date interval used in iterator and date period references
+	 *
+	 * @throws \Exception if `\DateInterval` cannot parse `$interval` parameter
 	 */
-    public function __construct($startDate, $endDate = null) {
-    	if (is_array($startDate) && count($startDate) > 1) {
-    		[$startDate, $endDate] = $startDate;
+	public function __construct($startDate, $endDate = null, string $interval = 'P1D')
+	{
+		if (is_array($startDate) && count($startDate) > 1) {
+			[$startDate, $endDate] = $startDate;
 		}
 
-    	$this->startDate = $this->parseDate($startDate);
+		$this->startDate = $this->parseDate($startDate);
 		$this->endDate = $this->parseDate($endDate);
 
 		/// Reverse times if end date is earlier than start date
@@ -80,17 +75,18 @@ class DateRange implements IteratorAggregate
 			$this->endDate = $tmp;
 		}
 
-        $this->interval = new DateInterval(self::INTERVAL);
-    }
+		$this->interval = new DateInterval($interval);
+	}
 
 	/**
 	 * Creates a `\DateTime` object from a timestamp or string. Returns existing `\DateTime` objects as-is.
+	 *
 	 * @param \DateTime|int|string $date Date object, timestamp, or date string
 	 *
 	 * @return \DateTime A `\DateTime` object set to the date defined in the `$date` parameter
 	 * @throws \InvalidArgumentException if `$date` can not be parsed by `\DateTime::__construct`
 	 */
-    private function parseDate($date): DateTime
+	private function parseDate($date): DateTime
 	{
 		if ($date instanceof DateTime) {
 			return $date;
@@ -114,173 +110,155 @@ class DateRange implements IteratorAggregate
 	}
 
 	/**
-	 * Sets flag to exclude start date in date period
-	 * @return \jasonjgardner\DateRange\DateRange
-	 * @throws \LogicException if the end date has already been excluded
-	 */
-    public function excludeStartDate(): self
-    {
-    	if ($this->excludeEndDate) {
-    		throw new LogicException('Can not exclude start date. Only one date may be excluded.');
-		}
-
-        $this->excludeStartDate = true;
-
-    	return $this;
-    }
-
-	/**
-	 * Sets flag to exclude end date in date period
-	 * @return \jasonjgardner\DateRange\DateRange
-	 * @throws \LogicException if the start date has already been excluded
-	 */
-    public function excludeEndDate(): self
-    {
-		if ($this->excludeEndDate) {
-			throw new LogicException('Can not exclude end date. Only one date may be excluded.');
-		}
-
-        $this->excludeEndDate = true;
-
-		return $this;
-    }
-
-	/**
-	 * Define the interval to use when determining the date range period
-	 * @param \DateInterval $interval
-	 *
-	 * @return \jasonjgardner\DateRange\DateRange
-	 */
-    public function setInterval(DateInterval $interval): self
-    {
-        $this->interval = $interval;
-
-        return $this;
-    }
-
-	/**
 	 * Gets the `\DateInterval` instance used in determining the date range period
 	 * @return \DateInterval
 	 */
-    public function getInterval(): DateInterval
-    {
-        return $this->interval;
-    }
+	public function getInterval(): DateInterval
+	{
+		return $this->interval;
+	}
+
+	/**
+	 * Define the interval to use when determining the date range period
+	 *
+	 * @param string|\DateInterval $interval `\DateInterval` instance or the interval spec string
+	 *
+	 * @return \jasonjgardner\DateRange\DateRange
+	 * @throws \Exception if `$interval` is an invalid spec string
+	 */
+	public function setInterval($interval): self
+	{
+		if (!$interval instanceof DateInterval) {
+			$interval = new DateInterval((string) $interval);
+		}
+
+		$this->interval = $interval;
+
+		return $this;
+	}
+
+	/**
+	 * Allow iterating over this date range based on the `\DateInterval` stored in `$this->interval`
+	 * @return \DatePeriod
+	 */
+	public function getIterator(): DatePeriod
+	{
+		return $this->getDatePeriod();
+	}
 
 	/**
 	 * Gets the date period between the start and end dates
+	 *
 	 * @param \DateInterval|null $interval Interval between dates. Defaults to `$this->interval` if `null`
+	 * @param int|null           $exclude  Optional bit flag which sets the date range to be inclusive or exclusive of
+	 *                                     the start and end dates themselves.
 	 *
 	 * @return \DatePeriod Date period between date range dates
 	 */
-    public function getDatePeriod(?DateInterval $interval = null): DatePeriod
-    {
+	public function getDatePeriod(?DateInterval $interval = null, ?int $exclude = null): DatePeriod
+	{
 		$endDate = $this->endDate;
 
-        if (!$this->excludeEndDate) {
-            $endDate = clone $this->endDate;
-            /// DatePeriod does not include end date so, plus 1 sec to end date.
-            $endDate->modify('+1 sec');
-        }
+		if (~$exclude & self::EXCLUDE_END_DATE) {
+			$endDate = clone $this->endDate;
+			/// DatePeriod does not include end date so, plus 1 sec to end date.
+			$endDate->modify('+1 sec');
+		}
 
-        $option = null;
+		$options = null;
 
-        if ($this->excludeStartDate) {
-            $option = DatePeriod::EXCLUDE_START_DATE;
-        }
+		if ($exclude & self::EXCLUDE_START_DATE) {
+			$options = DatePeriod::EXCLUDE_START_DATE;
+		}
 
-        return new DatePeriod(
-        	$this->startDate,
+		return new DatePeriod(
+			$this->startDate,
 			$interval ?? $this->interval,
 			$endDate,
-			$option
+			$options
 		);
-    }
-
-	/**
-	 * @return \DatePeriod
-	 */
-    public function getIterator(): DatePeriod
-    {
-        return $this->getDatePeriod();
-    }
+	}
 
 	/**
 	 * Get the earliest date as a `\DateTime` object
 	 * @return \DateTime
 	 */
-    public function getStartDate(): DateTime
-    {
-        return $this->startDate;
-    }
+	public function getStartDate(): DateTime
+	{
+		return $this->startDate;
+	}
 
 	/**
 	 * Get the latest date as a `\DateTime` object
 	 * @return \DateTime
 	 */
-    public function getEndDate(): DateTime
-    {
-        return $this->endDate;
-    }
+	public function getEndDate(): DateTime
+	{
+		return $this->endDate;
+	}
 
 	/**
 	 * Get the difference between the start and end dates
 	 * @return \DateInterval `\DateTime` difference
 	 */
-    public function diff(): DateInterval
-    {
-        return $this->startDate->diff($this->endDate);
-    }
+	public function diff(): DateInterval
+	{
+		return $this->startDate->diff($this->endDate);
+	}
 
 	/**
-	 * Checks if a date is within the date range
-	 * @param string|int|\DateTime $date A date to compare to the start and end dates
+	 * Checks if a date is within the date range. Date range includes the start and end dates by default.
+	 *
+	 * @param string|int|\DateTime $date    A date to compare to the start and end dates
+	 * @param int|null             $exclude Optional bit flag which sets the date range to be inclusive or exclusive of
+	 *                                      the start and end dates themselves.
 	 *
 	 * @return bool Returns `true` if `$date` is between the start and end dates
 	 * @throws \InvalidArgumentException if `$date` cannot be parsed
 	 */
-    public function contains($date): bool
-    {
-    	$date = $this->parseDate($date);
+	public function contains($date, ?int $exclude = null): bool
+	{
+		$date = $this->parseDate($date);
 
-		$isAfterThanStart = $this->startDate <= $date;
-		$isBeforeThanEnd = $date <= $this->endDate;
+		$isLaterThanStart = $this->startDate <= $date;
+		$isEarlierThanEnd = $date <= $this->endDate;
 
-        if ($this->excludeStartDate) {
-            $isAfterThanStart = $this->startDate < $date;
-        }
+		if ($exclude & self::EXCLUDE_START_DATE) {
+			$isLaterThanStart = $this->startDate < $date;
+		}
 
-        if ($this->excludeEndDate) {
-            $isBeforeThanEnd = $date < $this->endDate;
-        }
+		if ($exclude & self::EXCLUDE_END_DATE) {
+			$isEarlierThanEnd = $date < $this->endDate;
+		}
 
-        return $isAfterThanStart && $isBeforeThanEnd;
-    }
-
-	/**
-	 * Output date range dates as a formatted date string
-	 * @param string $format    PHP date format
-	 * @param string $separator String to place between formatted dates
-	 *
-	 * @return string Formatted date range dates
-	 */
-    public function toString(string $format = 'Y-m-d', string $separator = '~'): string
-    {
-        return sprintf(
-        	'%s %s %s',
-			$this->startDate->format($format),
-			$separator,
-			$this->endDate->format($format)
-		);
-    }
+		return $isLaterThanStart && $isEarlierThanEnd;
+	}
 
 	/**
 	 * Return formatted date range when cast as string
 	 * @return string Formatted date range dates
 	 * @uses \jasonjgardner\DateRange\DateRange::toString()
 	 */
-    public function __toString(): string
-    {
-        return $this->toString();
-    }
+	public function __toString(): string
+	{
+		return $this->toString();
+	}
+
+	/**
+	 * Creates a string representing the date range
+	 *
+	 * @param string      $startFormat Format in which to display the start date
+	 * @param null|string $endFormat   Format in which to display the end date. Defaults to `$startDate` if `null`
+	 * @param string      $strFormat   `sprintf` format used to combine dates
+	 *
+	 * @return string Start and end date formatted according to parameters
+	 */
+	public function toString(string $startFormat = 'Y-m-d', ?string $endFormat = null, string $strFormat = '%s - %s'): string
+	{
+		return sprintf(
+			$strFormat,
+			$this->startDate->format($startFormat),
+			$this->endDate->format($endFormat ?? $startFormat)
+		);
+	}
 }
